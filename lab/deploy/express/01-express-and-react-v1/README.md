fa154ab08c82f1471366718f396ca5ae881ac7f9
Jing <mqjing@gmail.com>
2021-06-11 20:44:29 +0800

    [express] Express + React

---
+++ lab/deploy/express/01-express-and-react-v1/README.md
@@ -0,0 +1,21 @@
+# Install
+```bash
+yarn --cwd ./server install
+yarn --cwd ./app install
+yarn --cwd ./app build
+```
+
+# Run
+```bash
+cd server
+tsc
+node index.js
+```
+
+# Test
+http://localhost:9000
+
+# Clean
+```bash
+./makeclean.sh
+```
--- lab/deploy/express/01-express-and-react-v1/server/index.ts
+++ lab/deploy/express/01-express-and-react-v1/server/index.ts
@@ -4,12 +4,11 @@ import path from 'path';
 const app = express();
 
 // Register a middleware to serve files from the React production build folder: ../build
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '../app/build')));
 
 // Register a middleware to handle GET request on the '/' to response the the React production index.html
 app.get('/', function (req, res) {
-  // res.sendFile(path.join(__dirname, '../build/index.html'));
-  res.sendFile(path.join(__dirname, '../build/', 'index.html'));
+  res.sendFile(path.join(__dirname, '../app/build/index.html'));
 });
 
 app.listen(9000);
