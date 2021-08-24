d0c16105181f69fa096d6c7c20aaed01a6b0843c
jing-tw <mqjing@gmail.com>
2021-08-25 01:54:18 +0800

    add ListGuesser

--- lab/react-admin/01-tutorial/test-admin/src/App.tsx
+++ lab/react-admin/01-tutorial/test-admin/src/App.tsx
@@ -1,10 +1,20 @@
-import * as React from "react";
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 // data source
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-// bootstrap
-const App = () => <Admin dataProvider={dataProvider} />;
+// Admin component
+const App = () => (
+  <Admin dataProvider={dataProvider}> 
+
+    {/* 
+    1. Fetch the record with the name as  "users" from ${dataProvider}/users URL
+    2. and list the record using ListGuesser component to display it */}
+    <Resource name="users" list={ListGuesser} />
+
+  </Admin>
+  
+  );
+
 export default App;
