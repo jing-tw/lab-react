adcd775d8b556dc80e208cc59f7efccb0daad2dd
jing-tw <mqjing@gmail.com>
2021-10-02 23:48:28 +0800

    [add] react-native-video player example

---
+++ lab/native/01-rn-videoplayer/VideoPlayer/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import VideoPlayer from './components/VideoPlayer';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <VideoPlayer />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+});
+
+export default App;
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+// import useCachedResources from './hooks/useCachedResources';
+// import useColorScheme from './hooks/useColorScheme';
+// import Navigation from './navigation';
+
+// export default function App() {
+//   const isLoadingComplete = useCachedResources();
+//   const colorScheme = useColorScheme();
+
+//   if (!isLoadingComplete) {
+//     return null;
+//   } else {
+//     return (
+//       <SafeAreaProvider>
+//         <Navigation colorScheme={colorScheme} />
+//         <StatusBar />
+//       </SafeAreaProvider>
+//     );
+//   }
+// }
