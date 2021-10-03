b5d3c70748c49ed99d2576272df0caf6e6470787
jing-tw <mqjing@gmail.com>
2021-10-03 14:09:14 +0800

    Add expo-av example basic

---
+++ lab/native/02-rn-videoplayer-expo-av-basic/VideoPlayer/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import { Audio, Video } from 'expo-av';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Video
+	  source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+          shouldPlay
+	  resizeMode="cover"
+	  style={{ width:300, height: 300 }}
+	/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
