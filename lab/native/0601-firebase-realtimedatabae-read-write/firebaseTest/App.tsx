bdb30daf25ee95c54d329cbe39e78ee38b88fe31
jing-tw <mqjing@gmail.com>
2021-11-15 14:36:20 +0800

    [Add] example to read/write firelbase realtime database

---
+++ lab/native/0601-firebase-realtimedatabae-read-write/firebaseTest/App.tsx
@@ -0,0 +1,163 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useRef, useState } from 'react'
+import { Dimensions, StyleSheet, Text, ScrollView, View, Button, Alert } from 'react-native';
+
+import { Video } from 'expo-av'
+
+import firebase from 'firebase'
+// import {initializeApp} from 'firebase/app';
+// import { getDatabase, ref, set, onValue } from 'firebase/database';
+
+// Initialize Firebase
+// Check the Project Filebase or 
+// here: 
+const firebaseConfig = {
+  apiKey: "AIzaSyCe41jTg0M-QEWEoGLkMaOCT0mQFXe8neY",
+  authDomain: "my-filebase-project.firebaseapp.com",
+  databaseURL: "https://my-filebase-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my-filebase-project",
+  storageBucket: "my-filebase-project.appspot.com",
+  messagingSenderId: "236920940780",
+  appId: "1:236920940780:web:16f1a1ecdcf0f354091dc9"
+};
+
+
+// https://stackoverflow.com/questions/43331011/firebase-app-named-default-already-exists-app-duplicate-app
+if (!firebase.apps.length) {
+  Alert.alert('new initial firebase app');
+  firebase.initializeApp(firebaseConfig);
+} else {
+  Alert.alert('if already initialized, use that one');
+  firebase.app(); // if already initialized, use that one
+}
+
+// firebase.initializeApp(firebaseConfig);
+
+function storeHighScore(userId: any, score: any) {
+  // Ref: 
+  // 1. https://firebase.google.com/docs/reference/node/firebase.database.Database
+  // 2. https://firebase.google.com/docs/reference/js/v8/firebase.database.Database#ref
+
+  const db = firebase.database();
+  var rootRef = firebase.database().ref();
+  var userRef = rootRef.child('users/' + userId);
+  // var userIdRef = firebase.database().ref('users/' + userId);
+
+  userRef.set({ highscore: score }, () => { Alert.alert('write ok.') })
+  // const reference = ref(db, 'users/' + userId);
+  // set(ref(db, 'users/' + userId), {
+  //   highscore: score,
+  // });
+}
+
+function getHighScore(userId:any) {
+  // ref. https://firebase.google.com/docs/database/web/read-and-write#web-version-8
+  const db = firebase.database();
+  var rootRef = firebase.database().ref();
+  var userRef = rootRef.child('users/' + userId);
+
+  userRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    Alert.alert('read data = ' + JSON.stringify(data));
+  });
+}
+
+
+// Alert.alert('Play and seek to 100ms')
+// storeHighScore('1234', '1234');
+// Alert.alert('store high score');
+
+const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get("window");
+
+export default function App() {
+  const video = React.useRef(null as any);
+  const [status, setStatus] = React.useState({} as any);
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, { fontWeight: 'bold', textTransform: 'uppercase' }]}>
+        Examples 1
+      </Text>
+
+      <Video
+        ref={video}
+        style={{ width: DEVICE_WIDTH, height: 300 }}
+        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+        shouldPlay
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+
+      {/* Control button view */}
+      <View style={styles.viewControlButton}
+      >
+
+        <Button
+          title={'write'}
+          onPress={() => {
+            storeHighScore('testUserId', '1234');
+          }
+          }
+        />
+
+<Button
+          title={'read'}
+          onPress={() => {
+            getHighScore('testUserId');
+          }
+          }
+        />
+
+
+        {/* Play button */}
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+
+        {/* Seek button */}
+        <Button
+          title={'Seek'}
+          onPress={() => {
+            if (status.isPlaying) {
+              video.current.setPositionAsync(2000);
+            } else {
+              Alert.alert('Click [Play]');
+            }
+
+
+          }
+          }
+        />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  viewControlButton: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    marginTop: 36,
+    marginBottom: 12,
+  },
+});
---
+++ lab/native/0601-firebase-realtimedatabae-read-write/firebaseTest/README.md
@@ -0,0 +1,30 @@
+# An example App that access Firebase realtime database using React expo.
+Here, I use the Firebase javascript verion 8 to do the tasks. 
+
+## Setup the Firebase config
+## Install & Run
+```bash
+expo init  firebaseTest  # create a new project
+cd firebaseTest
+expo install expo-av @react-native-community/slider    # install dependences
+
+yarn install firebase
+expo start
+```
+
+## Clean
+```bash
+rm -fr node_modules
+```
+
+## References
+1. https://console.firebase.google.com/
+2. https://docs.expo.dev/guides/using-firebase/
+3. https://medium.com/swlh/lets-create-mobile-app-with-react-native-and-firebase-6967a7946408
+4. https://firebase.google.com/docs/database/web/read-and-write#web-version-8
+5. Firebase, v8, https://firebase.google.com/docs/reference/js
+6. V9 supported issue, https://stackoverflow.com/questions/69236123/firebase-modular-v9-incompatible-with-expo
+
+
+
+
